2e6731dfd859e41aa8ad28bc6fa90e63d7d98841
Ianioglo Vladimir <skypromd@gmail.com>
2024-10-17 21:30:04 +0100

    Изменения в tests/test_decorators.py: обновлены тесты для декораторов

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,8 +1,11 @@
-import pytest
 import logging
-from unittest.mock import patch, MagicMock
-from src.decorators import log
 import sys
+from unittest.mock import patch
+
+import pytest
+
+from src.decorators import log
+
 
 # Пример функции для тестирования
 @log()
@@ -16,39 +19,43 @@ def function_with_error(x, y):
 
 
 def test_sample_function_logs_success():
-    with patch('logging.info') as mock_logging:
+    with patch("logging.info") as mock_logging:
         result = sample_function(3, 5)
         assert result == 8
-        mock_logging.assert_called_once_with('sample_function ok')
+        mock_logging.assert_called_once_with("sample_function ok")
 
 
 def test_function_with_error_logs_error():
-    with patch('logging.error') as mock_logging:
+    with patch("logging.error") as mock_logging:
         with pytest.raises(ZeroDivisionError):
             function_with_error(5, 0)
         mock_logging.assert_called_once()
 
 
 def test_custom_log_filename():
-    with patch('logging.basicConfig') as mock_logging_config:
-        @log(filename='test.log')
+    with patch("logging.basicConfig") as mock_logging_config:
+
+        @log(filename="test.log")
         def another_sample_function():
             return "done"
 
         another_sample_function()
-        mock_logging_config.assert_called_once_with(filename='test.log', level=logging.INFO,
-                                                    format="%(asctime)s - %(message)s")
+        mock_logging_config.assert_called_once_with(
+            filename="test.log", level=logging.INFO, format="%(asctime)s - %(message)s"
+        )
 
 
 def test_log_no_filename():
-    with patch('logging.basicConfig') as mock_logging_config:
+    with patch("logging.basicConfig") as mock_logging_config:
+
         @log()
         def another_function():
             return "done"
 
         another_function()
-        mock_logging_config.assert_called_once_with(stream=sys.stdout, level=logging.INFO,
-                                                    format="%(asctime)s - %(message)s")
+        mock_logging_config.assert_called_once_with(
+            stream=sys.stdout, level=logging.INFO, format="%(asctime)s - %(message)s"
+        )
 
 
 if __name__ == "__main__":
